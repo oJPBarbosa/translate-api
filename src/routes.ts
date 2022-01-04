531f965ef798ab0920ff3a4ad7efefab3e626ae6
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-04 17:40:11 -0300

    feat(api): migrate from /translate to /

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,6 @@ import { translateTextController } from './usecases/Translate/TranslateText'
 
 const router: Router = Router()
 
-router.post('/translate', (request, response) => translateTextController.handle(request, response));
+router.post('/', (request, response) => translateTextController.handle(request, response));
 
 export default router
