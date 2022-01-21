242760bba999b0b92ab87a96b1580680e3e1f218
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-21 12:27:47 -0300

    chore(api): linting

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,6 @@ import { translateTextController } from './usecases/Translate/TranslateText';
 
 const router: Router = Router();
 
-router.post('/', (request, response) =>
-  translateTextController.handle(request, response),
-);
+router.post('/', (request, response) => translateTextController.handle(request, response));
 
 export default router;
